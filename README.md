7cb7a454914680ef56dda4bc147220efc05895c0
ChristineZhiMei <2977797064@qq.com>
2025-11-16 03:52:27 +0800

    feat

---
+++ README.md
@@ -0,0 +1,20 @@
+# 二次封装组件已经一些工具
+```
+gc-kit/
+├── packages/           # 组件源代码
+│   ├── components/     # 组件目录
+│   │   ├── DynamicFiltering/   # 示例组件
+│   │   │   ├── src/
+│   │   │   │   ├── components/
+│   │   │   │   │   └── ...
+│   │   │   │   ├── index.vue
+│   │   │   └── index.ts
+│   └── index.ts        # 组件库入口文件
+├── vite.config.ts      # Vite 配置
+├── tsconfig.app.json   # TypeScript 配置
+├── tsconfig.json       # TypeScript 配置
+├── tsconfig.node.json  # TypeScript 配置
+├── README.md           # 说明文档
+├── .gitignore          # git 忽略文件
+└── package.json        # 项目依赖配置文件
+```
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'packages/index.ts'),
+      name: 'GcKit',
+      fileName: format => `gc-kit.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue','element-plus'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+          'element-plus': 'ElementPlus'
+        }
+      }
+    }
+  }
+})
